4d45ca5a9181552c1f4dc38ad17f13a642420284
kei <kei.yamada2023@gmail.com>
2023-10-02 22:24:29 +0900

    update from code

--- RoundnessForAI.js
+++ RoundnessForAI.js
@@ -2,9 +2,11 @@
 //Roundness calculation tool for Adobe Illustrator
 //Roundness is defined as diameter of largest inscribed circle divided by the average of the small circles.
 //This code measures diameters of circles. This code define the longest path of circle as maximum inscribed circle and the others as the small inscribed circle.
+//https://ai-scripting.docsforadobe.dev/index.html
 main();
 
 function main() {
+    var mmperpix = 2.83465;
     //check num of opened files
     if (documents.length < 1) {
         //if no file opeded
@@ -37,14 +39,8 @@ function main() {
     var PATH_LENGTH = [];
     var r = 0;
     for (i = 0; i < selected_paths.length; i++) {
-        path_diameter = selected_paths[i].length / (Math.PI);//diameter of circle
+        path_diameter  = (selected_paths[i].length / (Math.PI))/mmperpix;//diameter of circle
         PATH_LENGTH[i] = Math.round(path_diameter * 100) / 100;//Round to 2 decimal places
-        //get_path_location(selected_paths[i]);
-
-        for (n = 0; n < selected_paths[i].pathPoints.length; n++) {
-            alert(selected_paths[i].pathPoints[n]);
-        }
-        
     }
 
     //sort by largest
@@ -66,10 +62,11 @@ function main() {
 
     var R = r / PATH_LENGTH[0];//calc roundness
 
-    show_alert("Roundness: " + Math.round(R * 1000) / 1000 + "\r\n Max diameter: " + PATH_LENGTH[0] + "\r\n Mean small diameter: " + r + "\r\n\r\n All data: \r\n[" + (PATH_LENGTH).join('], [')+"]");//show result on prompt
+    //add value text
+    plot_roundness(R, selected_paths);
 
+    show_alert("Roundness: " + Math.round(R * 1000) / 1000 + "\r\n Max diameter: " + PATH_LENGTH[0] + " mm\r\n Mean small diameter: " + r + " mm\r\n\r\n All data: \r\n[" + (PATH_LENGTH).join(' mm], [')+" mm]");//show result on prompt
 }
-
 
 function extract_paths(obj, paths) {
     var min_path_points = 1;//if line, points are more than 2.
@@ -93,11 +90,20 @@ function extract_paths(obj, paths) {
     return paths;
 }
 
-function get_path_location(paths) {
+function get_paths_center(paths) {
+    var cx = 0;
+    var cy = 0;
+    var n  = 0;
     for (var i = 0; i < paths.length; i++) {
-        //paths[i].position
-        //alert(paths[i].pathItem.position);
+        for (var p = 0; p<paths[i].pathPoints.length;p++){
+            cx += paths[i].pathPoints[p].anchor[0];
+            cy += paths[i].pathPoints[p].anchor[1];
+            n  += 1;
+        }
     }
+    cx /= n;
+    cy /= n;
+    return [cx,cy];
 }
 
 function show_alert(txt) {
@@ -114,4 +120,23 @@ function show_alert(txt) {
     };
 
     dialog.show();
+}
+
+function plot_roundness(rval, selected_paths){
+    var cxy = get_paths_center(selected_paths);
+
+    var txtObj = activeDocument.textFrames.add();
+    txtObj.left = cxy[0];
+    txtObj.top = cxy[1];
+
+    txtObj.contents = "R="+Math.round(rval * 1000) / 1000;
+    txtObj.paragraphs[0].size = 10;
+
+    var txtCol   = new RGBColor(); 
+    txtCol.red   = 255; 
+    txtCol.green = 0; 
+    txtCol.blue  = 0; 
+    //txtObj.textRange.characterAttributes.strokeColor = txtCol;
+    txtObj.textRange.characterAttributes.fillColor = txtCol;
+    txtObj.createOutline();
 }
